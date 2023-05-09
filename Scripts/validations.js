b4b61f24fd77d8821ae21d2a44d41b5f4202348a
orelNeim <neimanorel@gmail.com>
2023-05-09 12:47:52 -0400

    update validations

---
+++ Scripts/validations.js
@@ -0,0 +1,15 @@
+function validateName(firstName, lastName) {
+    const namePattern = /^[A-Za-z]{2,}\s[A-Za-z]{2,}$/;
+    return namePattern.test(`${firstName} ${lastName}`);
+}
+
+function validateEmail(email) {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+}
+
+function validateDateOfBirth(dateOfBirth) {
+    const dobPattern = /^(\d{2})\/(\d{2})\/(\d{2})$/;
+    return dobPattern.test(dateOfBirth)
+}
+
