dd60f103c355e347b9489a2ac03e6f3992b76299
orelNeim <neimanorel@gmail.com>
2023-05-10 11:57:58 -0400

    Merge branch 'phase-3-html' of https://github.com/franerdozain/wizard into phase-3-html

--- Scripts/validations.js
+++ Scripts/validations.js
@@ -12,3 +12,10 @@ function validateDateOfBirth(dateOfBirth) {
     const dobPattern = /^(\d{4})-(\d{2})-(\d{2})$/;
     return dobPattern.test(dateOfBirth);
 }
+
+
+function getHouseNumber(houseNumber) {
+    let houseNumber;
+        while (isNaN(houseNumber) || houseNumber <= 0);
+        return houseNumber;
+}
